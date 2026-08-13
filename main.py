b8d6f50518742ed9ea28de780f2e4a3746e8619b
Aylin :D <aylinjourney30@gmail.com>
2026-08-12 22:04:21 -0500

    phyton files

---
+++ main.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import GWCutilities as util
+
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', None)
+
+print("\n-----\n")
+
+#Create a variable to read the dataset
+df = pd.read_csv("heartDisease_2020_sampling.csv")
+
+print(
+    "We will be performing data analysis on this Indicators of Heart Disease Dataset. Here is a sample of it: \n"
+)
+
+#Print the dataset's first five rows
+print(df.head(
+))
+
+input("\n Press Enter to continue.\n")
+
+
+
+#Data Cleaning
+#Label encode the dataset
+df = util.labelEncoder(df, ["HeartDisease", "GenHealth","Smoking","AlcoholDrinking","Sex","PhysicalActivity","AgeCategory"])
+
+print("\nHere is a preview of the dataset after label encoding. \n")
+
+print(df.head())
+
+input("\nPress Enter to continue.\n")
+
+#One hot encode the dataset
+df= util.oneHotEncoder(df,["Race"])
+
+print(
+    "\nHere is a preview of the dataset after one hot encoding. This will be the dataset used for data analysis: \n"
+)
+
+
+input("\nPress Enter to continue.\n")
+
+
+
+#Creates and trains Decision Tree Model
+from sklearn.model_selection import train_test_split
+
+X = df.drop("HeartDisease", axis = 1)
+y = df["HeartDisease"]
+X_train, X_test, Y_train, Y_test = train_test_split(X, y,random_state = 3)
+print(X_train.head())
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(max_depth = 6, class_weight = "balanced")
+clf = clf.fit(X_train, Y_train)
+
+#Test the model with the testing data set and prints accuracy score
+
+test_predictions = clf.predict (X_test)
+print("TEST:")
+print(test_predictions)
+from sklearn.metrics import accuracy_score
+test_acc = accuracy_score(Y_test, test_predictions)
+
+print( "The accurancy with the testing data set of the Desicion Tree is:"+ str(test_acc))
+
+#Prints the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,test_predictions, labels =  [1,0])
+print("The confusion matrix of the tree is : ")
+print(cm)
+
+#Test the model with the training data set and prints accuracy score
+train_predictions = clf.predict (X_train)
+from sklearn.metrics import accuracy_score
+train_acc = accuracy_score(Y_train, train_predictions)
+print( "The accurancy with the training data set of the Desicion Tree is:"+ str(train_acc))
+
+input("\nPress Enter to continue.\n")
+
+
+
+#Prints another application of Decision Trees and considerations
+print("\n A Decision Tree would be helpful to predict whether a vehicle is likely to fail or need major repairs based on measurements and history. Some factors to be careful of are making sure the data set includes data from a wide variety of different models and types of cars, selecting only the measurements that are relevant so the model does not introduce bias, and balanced data is necesary to make sure the dataset isn't overload with only 'problem' cars.\n")
+
+
+
+
+
+#Prints a text representation of the Decision Tree
+print("\nBelow is a text representation of how the Decision Tree makes choices:\n")
+input("\nPress Enter to continue.\n")
+util.printTree(clf,X.columns)
